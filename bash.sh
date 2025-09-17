b478180bdc035a3b1f761bb4d750025828b6fb8f
marifer152000 <mafer18mendez20@gmail.com>
2025-09-16 19:58:35 -0600

    Añade mensaje de inicio: 'The practice started' en bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,5 @@
+echo 'The practice started'
+echo 'The practice started'
 #!/bin/bash 
 COUNTER=0 
 while [ $COUNTER -lt 10 ] 
@@ -5,3 +7,4 @@ do
    echo The counter is $COUNTER 
    let COUNTER=$COUNTER+1 
 done
+echo 'The practice started'
